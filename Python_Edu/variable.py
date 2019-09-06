1447992768d9fff91c7e794b44754a511b1adfc0
Ryuseo <rkd4apf@gmail.com>
2019-09-06 17:53:12 +0900

    print기능부터 주석기능까지

--- Python_Edu/variable.py
+++ Python_Edu/variable.py
@@ -1,6 +1,6 @@
 
-identity = '지구인'     #정체성 변수
-number_of_leg = 4       #다리 갯수 변수
+identity = '지구인'
+number_of_leg = 4
 print('안녕! 나는', identity, '이야. 나는 다리가 ', number_of_leg , '개 있어.')
 
 identity = '한국인'
