7069216cdc070661b1d819fca78c041ad0629957
Ryuseo <rkd4apf@gmail.com>
2019-09-06 16:38:36 +0900

    create

---
+++ Python_Edu/variable.py
@@ -0,0 +1,8 @@
+
+identity = '지구인'     #정체성 변수
+number_of_leg = 4       #다리 갯수 변수
+print('안녕! 나는', identity, '이야. 나는 다리가 ', number_of_leg , '개 있어.')
+
+identity = '한국인'
+number_of_leg = 2
+print('안녕! 나는', identity, '이야. 나는 다리가 ', number_of_leg , '개 있어.')
