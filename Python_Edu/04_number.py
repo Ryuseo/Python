f229a4c47ce144ff2e3b466771f853fd8c34438f
Ryuseo <rkd4apf@gmail.com>
2019-09-09 17:56:37 +0900

    Arithmetic operator, if

---
+++ Python_Edu/04_number.py
@@ -0,0 +1,21 @@
+
+my_name = 'Python' #문자열 (사람을 위한 텍스트를 프로그래머가 부르는 방법)
+my_age = 2019 - 1994 #숫자
+
+print(my_name, '응 어제', my_age, '살')
+
+my_next_age = my_age + 1
+
+print('내년에는', my_next_age, '살')
+
+multiply = 9 * 9 # = 81
+divide = 30 / 5 # = 6
+power = 2 ** 10 # = 1024
+reminder = 15 % 4 # = 3
+
+print(multiply, divide, power, reminder)
+
+text = '2015' + '1999'
+number = 2015 + 1999
+
+print(text, number)
